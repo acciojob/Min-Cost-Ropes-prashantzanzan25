04229d1ed03605d90215047bda0c47ca10437d61
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-29 17:24:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	if (!arr.length){ 
+		return 0;
+	}
+	arr.sort((a, b) => a - b);
+	
+	let minCost = 0;
+	for (let i = 1; i < arr.length; i++) {
+	    minCost += arr[i - 1] + arr[i];
+	}
+	return minCost;
 }
 
 module.exports=mincost;
